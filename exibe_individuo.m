9fbe4827c3830e5a9cea2724ae018132faf4b8e3
tiago0022 <tiago.araujo.neo@gmail.com>
2020-11-03 19:37:32 -0300

    plota 2d

--- exibe_individuo.m
+++ exibe_individuo.m
@@ -1,5 +1,6 @@
 function exibe_individuo(populacao, individuo)
 x = populacao(individuo,1);
 y = populacao(individuo,2);
-z = bird(x, y);
-plot3(x, y, z, 'x', 'Color','r','LineWidth', 1.5, 'MarkerSize', 8);
+% z = bird(x, y);
+% plot3(x, y, z, 'x', 'Color','r','LineWidth', 1.5, 'MarkerSize', 8);
+plot(x, y, 'x', 'Color','r','LineWidth', 1.5, 'MarkerSize', 8);
